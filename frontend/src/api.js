3410539c54d9d790d37da24ce4b7422a02506d1a
poothesh <pootheshmm@gmail.com>
2025-07-06 21:56:11 +0530

    Fix: Updated CORS settings and replaced local URLs with deployed links

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api", // your backend
+  // baseURL: "http://localhost:5000/api", // your backend
+  baseURL: "https://task-manager-cz1k.onrender.com/api",
   withCredentials: true, // this sends cookies
 });
 
